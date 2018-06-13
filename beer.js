43ede4097a7a09a6ee2056ae42e7e8f0afc79471
Alex Salisbury <asalisbury@gmail.com>
2018-06-12 20:12:20 -0400

    Add Shameless Green solution

--- beer.js
+++ beer.js
@@ -1,5 +1,37 @@
 class Bottles {
-  // TODO
+  constructor() {
+    this.verse = this.verse.bind(this);
+  }
+
+  song() {
+    return this.verses(99, 0);
+  }
+
+  verses(upper, lower) {
+    return Array.from({length: (upper - lower + 1)}, (_, index) => index + lower)
+      .reverse()
+      .map(this.verse)
+      .join("\n");
+  }
+
+  verse(number) {
+    switch (number) {
+      case 0:
+        return 'No more bottles of beer on the wall, no more bottles of beer.\n' +
+        'Go to the store and buy some more, 99 bottles of beer on the wall.\n'
+      case 1:
+        return '1 bottle of beer on the wall, 1 bottle of beer.\n' +
+        'Take it down and pass it around, no more bottles of beer on the wall.\n'
+      case 2:
+        return '2 bottles of beer on the wall, 2 bottles of beer.\n' +
+        'Take one down and pass it around, 1 bottle of beer on the wall.\n'
+      default:
+        return `${number} bottles of beer on the wall, ` +
+          `${number} bottles of beer.\n` +
+          `Take one down and pass it around, ` +
+          `${number - 1} bottles of beer on the wall.\n`;
+    }
+  }
 }
 
 module.exports = Bottles;
